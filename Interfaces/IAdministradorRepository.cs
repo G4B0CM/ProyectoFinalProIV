6da421838ef7d37b1ddb5bcaee527bcb53876495
EduSalazar0 <edutebansalazar95@gmail.com>
2024-12-08 21:15:18 -0500

    Se creo una carpeta de Interfaces en donde se añadio dos clases una IAdministrador y IEstudiante.

--- Interfaces/IAdministradorRepository.cs
+++ Interfaces/IAdministradorRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Avance2Progreso.Interfaces
 {
-    internal class IAdministradorRepository
+    public class IAdministradorRepository
     {
     }
 }
--- Interfaces/IEstudianteRepository.cs
+++ Interfaces/IEstudianteRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Avance2Progreso.Interfaces
 {
-    internal class IEstudianteRepository
+    public class IEstudianteRepository
     {
     }
 }
