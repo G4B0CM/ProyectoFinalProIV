e1df831ac60334146c4689cd10afc6e4fa82ed42
EduSalazar0 <edutebansalazar95@gmail.com>
2025-01-25 14:05:41 -0500

    Se incluyo etiquetas asociadas a SQLite en la clase competencia, Se creo una nueva clase Repository, para menejar el almacenamiento con la base de datos.

--- Models/Competencias.cs
+++ Models/Competencias.cs
@@ -1,6 +1,7 @@
 ﻿
 using SQLite;
 using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
 
 namespace Avance2Progreso.Models
 {
@@ -8,13 +9,18 @@ namespace Avance2Progreso.Models
     public class Competencias
     {
         [PrimaryKey, AutoIncrement]
+        [SQLite.Column("Id")]
         public int Id { get; set; }
         [Required(ErrorMessage ="Nombre Requerido")]
+        [SQLite.Column("Nombre")]
         public string Nombre { get; set; }
         [Required(ErrorMessage ="Categoria Requerida")]
+        [SQLite.Column("Categoria")]
         public string Categoria { get; set; }
         [Required(ErrorMessage ="Descripción Requerida")]
+        [SQLite.Column("Descripción")]
         public string Descripcion { get; set; }
+        [SQLite.Column("FechaCreación")]
         public DateTime FechaCreación { get; set; } 
     }
 }
---
+++ Repositories/CompetenciasRepository.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Avance2Progreso.Models;
+using Microsoft.Maui.Controls;
+using SQLite;
+
+namespace Avance2Progreso.Repositories
+{
+    public class CompetenciasRepository
+    {
+        string _dbPath;
+
+        public string StatusMessage { get; set; }
+
+        private SQLiteConnection conn;
+
+        private void Init()
+        {
+            if (conn != null)
+            {
+                return;
+
+                conn = new SQLiteConnection(_dbPath);
+                conn.CreateTable<Competencias>();
+            }
+
+        }
+        public CompetenciasRepository(string dbPath)
+        {
+            _dbPath = dbPath;
+        }
+        public async Task SaveCompetencia(string nombre, string categoria, string descripcion)
+        {
+            try
+            {
+                Init();
+                if (string.IsNullOrEmpty(nombre))
+                    throw new Exception("Se requiere un nombre valido");
+                if (string.IsNullOrEmpty(categoria))
+                    throw new Exception("Se requiere una region valida");
+                if (string.IsNullOrEmpty(descripcion))
+                    throw new Exception("Se requiere una region valida");
+
+                var result = conn.Insert(new Competencias
+                {
+                    Nombre = nombre,
+                    Categoria = categoria,
+                    Descripcion = descripcion,
+                    FechaCreación = ObtenerFecha()
+                });
+
+                StatusMessage = $"Competencia {nombre} guardada correctamente";
+
+
+            } catch (Exception ex) {
+                StatusMessage = $"Error al guardar la competencia. Detalles: {ex.Message}";
+            }
+        }
+        public async Task<List<Competencias>> GetAllCompetencias()
+        {
+            try
+            {
+                Init() ;
+                return conn.Table<Competencias>().ToList();
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = $"Error al listar las competencias";
+            }
+            return new List<Competencias>();
+        }
+
+        public static DateTime ObtenerFecha()
+        {
+            return DateTime.Now;
+        }
+
+
+    }
+}
